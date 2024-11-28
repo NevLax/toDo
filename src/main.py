9802850c14ef06574dcfc382a4698d947e44db16
NevLax <qwegornikitin@gmail.com>
2024-11-28 16:46:52 +0400

    add page for create todo and work markdown

--- src/main.py
+++ src/main.py
@@ -1,15 +1,19 @@
 from flask import Flask
 from flask import url_for
 from flask import request
+from flask import redirect
 from flask import render_template
+import markdown
 import uuid
 
 app = Flask(__name__)
 todo_dict = dict()
 #url_for('static', filename='style.css')
 
+
 def todo_simple(title='Title', description=None):
     return {'title': title, 'description': description}
+
 
 def impostor_list():
     todo_dict[str(uuid.uuid1())] = todo_simple(title='Simple')
@@ -18,18 +22,32 @@ def impostor_list():
 impostor_list()
 print(todo_dict)
 
+
 @app.route('/')
 def hello():
     return 'Hello'
+
 
 @app.route('/todo/', methods=['GET', 'POST'])
 def todo():
     if request.method == 'GET': #get all todo
         return render_template('todo.html', td_dict=todo_dict)
     else:                       #post for add new todo
-        todo_item = request.json['todo']
-        return redirect(url_for('todo')) #redirect to list item
+        todo_item = {
+            'title': request.form.get('title'),
+            'description': markdown.markdown(request.form.get('description'))
+        }
+        uid = str(uuid.uuid1())
+        todo_dict[uid] = todo_item
+        return redirect(url_for('get_todo', id=uid)) #redirect to list item
+
 
 @app.route('/todo/<uuid:id>/')
 def get_todo(id):
     return render_template('todo-item.html', item=todo_dict[str(id)])
+
+
+@app.route('/new-todo/')
+def new_todo():
+    return render_template('todo-form.html')
+    
