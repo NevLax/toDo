b5e130b7e164bd1a4f5884de3d2f4f8e052ecb6c
NevLax <qwegornikitin@gmail.com>
2024-11-30 23:32:29 +0400

    sqlite db, all work in this project(crud operation), but not realese update Notes

--- src/main.py
+++ src/main.py
@@ -3,24 +3,26 @@ from flask import url_for
 from flask import request
 from flask import redirect
 from flask import render_template
+import sqlite3
 import markdown
 import uuid
 
+
 app = Flask(__name__)
 todo_dict = dict()
-#url_for('static', filename='style.css')
 
 
-def todo_simple(title='Title', description=None):
-    return {'title': title, 'description': description}
-
-
-def impostor_list():
-    todo_dict[str(uuid.uuid1())] = todo_simple(title='Simple')
-    todo_dict[str(uuid.uuid1())] = todo_simple(title='Dimple')
-    todo_dict[str(uuid.uuid1())] = todo_simple(title='PopIt')
-impostor_list()
-print(todo_dict)
+connection = sqlite3.connect('database.db')
+connection.execute('''
+CREATE TABLE IF NOT EXISTS Notes (
+id INTEGER PRIMARY KEY,
+uuid TEXT NOT NULL,
+title TEXT NOT NULL,
+description TEXT
+)
+''')
+connection.commit()
+connection.close()
 
 
 @app.route('/')
@@ -31,28 +33,48 @@ def hello():
 @app.route('/todo/', methods=['GET', 'POST'])
 def todo():
     if request.method == 'GET': #get all todo
-        return render_template('todo.html', td_dict=todo_dict)
+        connect = sqlite3.connect('database.db')
+        cursor = connect.cursor()
+        cursor.execute('SELECT * FROM Notes')
+
+        data = cursor.fetchall()
+        print(data)
+        return render_template('todo.html', data=data)
     else:                       #post for add new todo
-        todo_item = {
-            'title': request.form.get('title'),
-            'description': markdown.markdown(request.form.get('description'))
-        }
+        title = request.form.get('title')
+        description = markdown.markdown(request.form.get('description'))
         uid = str(uuid.uuid1())
-        todo_dict[uid] = todo_item
-        return redirect(url_for('get_todo', id=uid)) #redirect to list item
+
+        with sqlite3.connect('database.db') as notes:
+            cursor = notes.cursor()
+            cursor.execute('INSERT INTO Notes (uuid, title, description) VALUES (?, ?, ?)',
+                (uid, title, description)
+            )
+            notes.commit()
+            
+        return redirect(url_for('get_todo', id=uid))
 
 
 @app.route('/todo/<uuid:id>/')
 def get_todo(id):
-    return render_template('todo-item.html', item=todo_dict[str(id)])
+    with sqlite3.connect('database.db') as notes:
+        cursor = notes.cursor()
+        cursor.execute('SELECT * FROM Notes WHERE uuid = ?', (str(id),))
+        
+        data = cursor.fetchone() 
+        return render_template('todo-item.html', item=data)
 
 
 @app.route('/todo/<uuid:id>/del', methods=['POST'])
 def delete_todo(id):
-    del todo_dict[str(id)]
-    return redirect(url_for('todo'))
+    with sqlite3.connect('database.db') as notes:
+        cursor = notes.cursor()
+        cursor.execute('DELETE FROM Notes WHERE uuid = ?', (str(id),) )
+
+        notes.commit()
+        return redirect(url_for('todo'))
+
 
 @app.route('/new-todo/')
 def new_todo():
     return render_template('todo-form.html')
-    
